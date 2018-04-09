6d3c935c431b858a99abd7aa5d0e339da360c6c9
jemmy li <zengarden2009@gmail.com>
2018-04-09 22:09:21 +0800

    update make.sh

--- lib/make.sh
+++ lib/make.sh
@@ -18,6 +18,14 @@ pushd lib_kernel/lib_roi_align
 sh make.sh
 popd
 
+pushd lib_kernel/lib_psalign_pooling
+sh make.sh
+popd
+
+pushd lib_kernel/lib_nms_dev
+sh make.sh
+popd
+
 pushd datasets_odgt/lib_coco/PythonAPI
 make install
 popd
