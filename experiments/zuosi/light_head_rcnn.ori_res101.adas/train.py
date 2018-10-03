50b6b9a1bf07d50fd7db439a4383a7bc40eaf628
LeftThink <zuosi8968@gmail.com>
2018-10-03 14:00:49 +0800

    optimize

--- experiments/zuosi/light_head_rcnn.ori_res101.adas/train.py
+++ experiments/zuosi/light_head_rcnn.ori_res101.adas/train.py
@@ -30,15 +30,17 @@ import pdb
 import warnings
 warnings.filterwarnings("ignore")
 
-def snapshot(sess, saver, epoch, step):
+def snapshot(sess, saver, epoch, iter, global_step):
     # 这是要一个epoch才保存一次的节奏啦
-    filename = 'epoch_{:d}'.format(epoch) + '.ckpt'
+    filename = 'epoch_{:d}_step_{:d}.ckpt'.format(epoch, iter)
     # e.g. output/zuosi/light_head_rcnn.ori_res101.coco
     if not os.path.exists(cfg.ckpt_dir):
         os.makedirs(cfg.ckpt_dir)
-    filename = os.path.join(cfg.ckpt_dir, filename)
-    saver.save(sess, filename, global_step=step)
-    print('Wrote snapshot to: {:s}'.format(filename))
+
+    filepath = os.path.join(cfg.ckpt_dir, filename)
+    saver.save(sess, filepath, global_step=global_step)
+
+    print('Wrote checkpoint: {}'.format(filename))
 
 
 def get_data_flow():
@@ -70,9 +72,8 @@ def train(args):
     # 意思是将简单的运算放在cpu上,只将神经网络的训练过程放在GPU上,不要误会!
     with tf.Graph().as_default(), tf.device('/cpu:0'):
         # 定义训练轮数
-        global_step = tf.get_variable(
-            'global_step', [], initializer=tf.constant_initializer(0.),
-            trainable=False)
+        #global_step = tf.get_variable('global_step', [], initializer=tf.constant_initializer(0.), trainable=False)
+        global_step = tf.get_variable('global_step', [], dtype=tf.int64, initializer=tf.constant_initializer(0), trainable=False)
 
         # log_device_placement=True会打印出执行操作所使用的设备 
         tfconfig = tf.ConfigProto(
@@ -203,7 +204,6 @@ def train(args):
         # 奇怪,因为variables这个变量等于tf.global_variables(),所以tf.variables_initializer(variables)
         # 就相当于tf.global_variables_initializer()
         sess.run(tf.variables_initializer(variables, name='init'))
-
         variables_to_restore = []
         for v in variables:
             # 为啥下面这样操作?因为v.name是变量名,e.g. v:0,v1:0
@@ -215,15 +215,13 @@ def train(args):
         # var_list指定将要保存/恢复的变量,它可以传递dict或list
         restorer = tf.train.Saver(variables_to_restore) # 为毛不是直接tf.train.Saver()
 
-        restore_from_checkpoint = False 
-        if args.checkpoint is not None:
-            match = re.findall(r'\d+', args.checkpoint)
-            if len(match) != 2:
-                raise Exception("invalid checkpoint: {}".format(args.checkpoint))
-            print('restore from checkpoint: {}'.format(args.checkpoint))
-            restorer.restore(sess, args.checkpoint)
-            start_epoch, start_step = [int(e) for e in match]
-            restore_from_checkpoint = True
+        restore_from_ckpt = False 
+        if args.resume:
+            checkpoint = 'epoch_{:d}_step_{:d}.ckpt'.format(args.checkepoch, args.checkstep)
+            print('restore from ckpt: {}'.format(os.path.join(cfg.ckpt_dir, checkpoint)))
+            restorer.restore(sess, os.path.join(cfg.ckpt_dir, checkpoint))
+            start_epoch, start_step = args.checkepoch, args.checkstep
+            restore_from_ckpt = True
         else:
             print('restore from pretrained file: {}'.format(cfg.weight))
             restorer.restore(sess, cfg.weight)
@@ -255,7 +253,7 @@ def train(args):
         print('##### start train #####')
         iters_per_epoch = cfg.nr_image_per_epoch // (num_gpu * cfg.train_batch_per_gpu) + 1
         for epoch in range(start_epoch, cfg.max_epoch):
-            if not restore_from_checkpoint and cfg.warm_iter > 0:
+            if not restore_from_ckpt and cfg.warm_iter > 0:
                 '''为啥有[bottom_lr,up_lr]这个区间?这里的学习率调整更平滑,注意还有一个iter_delta_lr是
                 每一轮迭代的学习率调整步长.'''
                 up_lr = cfg.get_lr(0) * num_gpu
@@ -264,7 +262,7 @@ def train(args):
                 cur_lr = bottom_lr
                 for iter in range(cfg.warm_iter):
                     sess.run(update_lr_op, {lr_placeholder: cur_lr})
-                    cur_lr += iter_delta_lr # 每一轮迭代的学习率调整步长,高级
+                    cur_lr += iter_delta_lr #每一轮迭代的学习率调整步长,高级
                     feed_dict = {}
                     blobs_list = prefetch_data_layer.forward()
                     for i, inputs in enumerate(inputs_list):
@@ -274,7 +272,7 @@ def train(args):
                             feed_dict[inputs[it_idx]] = blobs[it_inputs_name]
                     sess_ret = sess.run(sess2run, feed_dict=feed_dict)
 
-                    if iter % 100 == 0:
+                    if iter > 0 and iter % 100 == 0:
                         print_str = 'epoch %d, iter %d/%d, ' % (epoch, iter, cfg.warm_iter)
                         # train_collection.keys => ['rpn_loss _cls','rpn_loss_box','loss_cls','loss_box','tot_losses']
                         for idx_key, iter_key in enumerate(train_collection.keys()):
@@ -282,16 +280,18 @@ def train(args):
 
                         print_str += 'lr: %.4f, speed: %.3fs/iter' % (cur_lr, timer.average_time)
                         print(print_str)
+                # update start_step
+                start_step = cfg.warm_iter
 
             '''nr_image_per_epoch这个视数据集不同而不同,像coco2014这个数就是80k的样子,
             每个cpu装入train_batch_per_gpu张图片,总共num_gpu个gpu,以此来计算需要多
-            少次迭代, +1是因为range的原因'''
-            # 为了更新学习率,lr_placeholder是前面定义的一个占位符,通过这种方式在
-            # 运动中更新学习率,在不同的epoch阶段要适当调整学习率嘛,嘿嘿
+            少次迭代, +1是因为range的原因.为了更新学习率,lr_placeholder是前面定义的一个占位符,
+            通过这种方式在运动中更新学习率,在不同的epoch阶段要适当调整学习率嘛,嘿嘿'''
             cur_lr = cfg.get_lr(epoch) * num_gpu
             sess.run(update_lr_op, {lr_placeholder: cur_lr})
 
             loss_temp = 0.0
+
             if epoch > start_epoch:
                 start_step = 0
 
@@ -310,22 +310,22 @@ def train(args):
 
                 #因为'tot_losses'正好是最后一项,这里取巧才这么用,不规范
                 loss_temp += sess_ret[-1]
+                
+                gs = sess.run(global_step)
+                if gs % cfg.disp_interval == 0:
+                    print_str = 'epoch %d, iter %d/%d, ' % (epoch, iter, iters_per_epoch)
+                    for idx_key, iter_key in enumerate(train_collection.keys()):
+                        print_str += iter_key + ': %.4f, ' % sess_ret[idx_key + 2]
 
-                if iter != start_step:
-                    if iter % cfg.disp_interval == 0:
-                        print_str = 'epoch %d, iter %d/%d, ' % (epoch, iter, iters_per_epoch)
-                        for idx_key, iter_key in enumerate(train_collection.keys()):
-                            print_str += iter_key + ': %.4f, ' % sess_ret[idx_key + 2]
+                    print_str += 'tot_losses_mean: %.4f, ' % (loss_temp/cfg.disp_interval)
+                    print_str += 'lr: %.4f, speed: %.3fs/iter' % (cur_lr, timer.average_time)
+                    print(print_str)
 
-                        print_str += 'tot_losses_mean: %.4f, ' % (loss_temp/cfg.disp_interval)
-                        print_str += 'lr: %.4f, speed: %.3fs/iter' % (cur_lr, timer.average_time)
-                        print(print_str)
+                    loss_temp = 0.0
+                if gs % cfg.snapshot_interval == 0:
+                    snapshot(sess, saver, epoch, iter, global_step)
 
-                        loss_temp = 0.0
-                    if iter % cfg.snapshot_interval == 0:
-                        snapshot(sess, saver, epoch, global_step)
-
-            snapshot(sess, saver, epoch, global_step)
+            snapshot(sess, saver, epoch, iter, global_step)
         coord.request_stop() # 跑完了,请求停止
         coord.join(queue_runner) # 等待所有线程终止
 
@@ -335,9 +335,12 @@ if __name__ == '__main__':
     parser.add_argument(
         '-r', '--resume', default=False, action='store_true', help='restore from checkpoint or not')
     parser.add_argument(
-        '-c', '--checkpoint', default=None, type=str, help='checkpoint path')
-    parser.add_argument(
         '-d', '--devices', default='0', type=str, help='device for training')
+    parser.add_argument(
+        '-e', '--checkepoch', dest='checkepoch', type=int, help='checkepoch to load model', default=0)
+    parser.add_argument(
+        '-s', '--checkstep', dest='checkstep', type=int, help='checkstep to load model', default=0)
+
     args = parser.parse_args()
     
     args.devices = misc.parse_devices(args.devices)
